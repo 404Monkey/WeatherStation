06363bf537f3de7e34d98ebc375b145aef805176
Henintsoa <henintsoa.andrvny@gmail.com>
2021-12-07 11:08:04 +0100

    Standardization - V1

--- dev/Core/Src/Raingauge.c
+++ dev/Core/Src/Raingauge.c
@@ -1,3 +1,11 @@
+/**************************************************************
+   Raingauge, from WeatherStation library, is available for STM32F746G platform to manage
+   our connected WeatherStation.
+
+   Licensed under University of Poitiers M1 Connected Objects by TD1 GRP1.
+   Produced by Henintsoa Andrianarivony.
+ **************************************************************/
+
 #include "Raingauge.h"
 
 // - Start the timer dedicated to the rain gauge
@@ -7,7 +15,7 @@ void RaingaugeStart(TIM_HandleTypeDef *htim2){
 }
 
 // - Give the rainfall saved since the last call function
-double RaingaugeCaptureRainfall(TIM_HandleTypeDef *htim2, int delay){
+double captureRainfall(TIM_HandleTypeDef *htim2, int delay){
 	//Stock the value of the rain gauge counter and reset the counter
 	long counter = __HAL_TIM_GET_COUNTER(htim2);
 	__HAL_TIM_SetCounter(htim2,0);
@@ -16,19 +24,19 @@ double RaingaugeCaptureRainfall(TIM_HandleTypeDef *htim2, int delay){
 	if (delay == 0) {
 		//If so, display an error message and return an error value
 
-		RaingaugeDisplayRainfall(counter, RAINGAUGE_ERROR_VAL, 0);
+        displayRainfall(counter, RAINGAUGE_ERROR_VAL, 0);
 		return RAINGAUGE_ERROR_VAL;
 	} else {
 		//If not, compute rainfall then display and return it
 
 		double rainfall = counter * CAPACITY * 3600 / delay;
-		RaingaugeDisplayRainfall(counter, rainfall, delay);
+        displayRainfall(counter, rainfall, delay);
 		return rainfall;
 	}
 }
 
 // - Monitoring data via UART serial port
-void RaingaugeDisplayRainfall(long counter, double rainfall, int delay) {
+void displayRainfall(long counter, double rainfall, int delay) {
 	printf("====== MONITORING RAIN GAUGE ======\r\n");
 	if (delay == 0) {
 		printf("=> ERROR RAIN GAUGE : In function \"Raingauge_captureRainfall\", delay cannot be equal to 0. \r\n");
